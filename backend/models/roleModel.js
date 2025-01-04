b57b281396ff6dee9d967e713dab8c1a9869f1a1
Ozear Khan <ozearkhan1224@gmail.com>
2025-01-04 11:59:17 +0530

    made RBAC.js dynamic

--- backend/models/roleModel.js
+++ backend/models/roleModel.js
@@ -30,6 +30,28 @@ class RoleModel {
 
         return permissions.map(p => p.name);
     }
+
+    static async getAllRoles() {
+        const [roles] = await db.query('SELECT * FROM roles');
+        return roles;
+    }
+
+    static async getAllPermissions() {
+        const [permissions] = await db.query('SELECT * FROM permissions');
+        return permissions;
+    }
+
+    static async getRolePermissionsFromDB(roleName) {
+        const [permissions] = await db.query(`
+            SELECT p.name 
+            FROM permissions p
+            JOIN role_permissions rp ON p.id = rp.permission_id
+            JOIN roles r ON r.id = rp.role_id
+            WHERE r.name = ?
+        `, [roleName]);
+        
+        return permissions.map(p => p.name);
+    }
 }
 
 module.exports = RoleModel;
